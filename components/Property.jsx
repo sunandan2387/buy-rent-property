67c5f5d98654a7cce4c572a24c47efed0f5fe548
sunandan <sunandanlalka@gmail.com>
2022-12-24 00:42:09 +0530

    ready to deploy

--- components/Property.jsx
+++ components/Property.jsx
@@ -7,8 +7,8 @@ import { GoVerified } from 'react-icons/go'
 import millify from 'millify'
 import DefaultImage from '../assets/images/defaulthouse.jpg'
 
-const Property = ({property: {coverPhoto, price, rentFrequency, rooms, title, baths, area, agency, isVerified, externalId}}) => (
-    <Link href={`/property/${externalId}`} passHref>
+const Property = ({property: {coverPhoto, price, rentFrequency, rooms, title, baths, area, agency, isVerified, externalID}}) => (
+    <Link href={`/property/${externalID}`} passHref>
         <Flex flexWrap="wrap" w="420px" p="5" paddingTop="0" justifyContent="flex-start" cursor="pointer">
             <Box>
                 <Image src={coverPhoto ? coverPhoto.url : DefaultImage} width={400} height={260} alt="house"/>
@@ -17,7 +17,7 @@ const Property = ({property: {coverPhoto, price, rentFrequency, rooms, title, ba
                 <Flex paddingTop="2" alignItems="center" justifyContent="space-between">
                     <Flex alignItems="center">
                         <Box paddingRight="3" color="green.400">{isVerified && <GoVerified />}</Box>
-                        <Text fontWeight="bold" fontSize="lg">Rs {millify(price)}{rentFrequency && `/${rentFrequency}`}</Text>
+                        <Text fontWeight="bold" fontSize="lg">$ {millify(price)}{rentFrequency && `/${rentFrequency}`}</Text>
                     </Flex>
                     <Box>
                         <Avatar size="sm" src={agency?.logo?.url}/>
